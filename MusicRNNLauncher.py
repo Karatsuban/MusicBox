079800bfcecde9f4296d8c7426f09fedf4cf74d8
Karatsuban <75843468+Karatsuban@users.noreply.github.com>
2021-05-06 12:30:59 +0200

    Add files via upload
    Changement de l'appel de InterfaceChoixParametres en appel à Application

--- MusicRNNLauncher.py
+++ MusicRNNLauncher.py
@@ -1,8 +1,8 @@
 # !/usr/bin/python3
 #coding: utf-8
-from source.view.scripts import InterfaceChoixParametres, InterfaceLecteur
+from source.view.scripts import Application
 import Install
 
 if __name__ == "__main__":
 	Install.installRequiredPackages()
-	InterfaceChoixParametres.start()
+	Application.start()
