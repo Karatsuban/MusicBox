5930bd444d8be4f01fb1e61cf9e51e6bfaa5a388
Karatsuban <75843468+Karatsuban@users.noreply.github.com>
2021-05-27 13:22:57 +0200

    Add files via upload
    -Ajout du bouton "Accéder au dossier" pour ouvrir directement le dossier où sont stockés les résultats
    -Ajout de la méthode opendir() qui ouvre le dossier choisi

--- source/view/scripts/Lecteur.py
+++ source/view/scripts/Lecteur.py
@@ -1,5 +1,5 @@
 # !/usr/bin/python3
-#coding: utf-8
+# coding: utf-8
 
 import tkinter
 import tkinter.font as tkFont
@@ -7,8 +7,6 @@ from tkinter import ttk, messagebox
 import pygame
 import os
 
-
-hauteurBout = 10
 largeurBout = 15
 hauteurBout = 2
 margeX = 0
@@ -21,177 +19,160 @@ margeY = 0
 
 class Lecteur(tkinter.Frame):
     def __init__(self, master, parametres):
-        #Initialisation du Cadre du lecteur MP3
+        # Initialisation du Cadre du lecteur MP3
         tkinter.Frame.__init__(self, master)
 
         self.parametres = parametres
 
-        #URL du répertoire Resultat
-        self.URL = self.parametres["URL_Dossier"]+os.sep+"Resultat"+os.sep
+        # URL du répertoire Resultat
+        self.URL = self.parametres["URL_Dossier"] + os.sep + "Resultat" + os.sep
 
-        self.userReponse = None #pour gérer le interactions avec les utilisateurs
+        self.userReponse = None  # pour gérer le interactions avec les utilisateurs
 
-        #Réglages de la police du titre puis du texte
+        # Réglages de la police du titre puis du texte
         self.titre = tkFont.Font(family='Helvetica', size=20)
         self.texte = tkFont.Font(family='Helvetica', size=13)
 
-        #Couleur de fond
+        # Couleur de fond
         self.configure(bg='white')
 
         # variables utiles pour la gestion des musiques
         self.numeroTitre = 0
         self.listeMusiques = []
-        self.lecteurState = "Stop" #peut avoir les états "Pause", "Play", "Stop"
+        self.lecteurState = "Stop"  # peut avoir les états "Pause", "Play", "Stop"
 
+        # Initialisation du lecteur
+        initLecteur()
+        self.master.protocol('WM_DELETE_WINDOW', lambda: [pygame.mixer.music.stop(), self.master.destroy()])
 
-        #Initialisation du lecteur 
-        self.initLecteur()
-        self.master.protocol('WM_DELETE_WINDOW', lambda : [pygame.mixer.music.stop(), self.master.destroy()])
+        # Titre
+        tkinter.Label(self, text="Résultat de la génération", font=self.titre, height=hauteurBout, bg='white').grid(row=0, column=0, sticky="W")
+        # Titre de la musique en cours
+        self.boxTitreMusique = tkinter.Label(self, text="Titre: ", bg='white', font=self.texte).grid(row=1, column=0, sticky="w")
 
-        #Titre
-        resultatLabel = tkinter.Label(self, text="Résultat de la génération", font = self.titre, height = hauteurBout, bg='white').grid(row = 0, column = 0, sticky="W")
-        #Titre de la musique en cours
-        self.boxTitreMusique = tkinter.Label(self, text="Titre: ", bg='white', font = self.texte).grid(row=1,column=0, sticky="w")
-
-
-        #Combobox listant tous les titres
+        # Combobox listant tous les titres
         self.titreMusique = ""
-        self.comboTitres = ttk.Combobox(self,state="readonly", width=30)
-        #Placement
+        self.comboTitres = ttk.Combobox(self, state="readonly", width=30)
+        # Placement
         self.comboTitres.grid(row=1, column=0, columnspan=3)
-        self.comboTitres.bind("<<ComboboxSelected>>", lambda e:[master.focus(),self.selectionMusique("<<ComboboxSelected>>")])
+        self.comboTitres.bind("<<ComboboxSelected>>", lambda e: [master.focus(), self.selectionMusique("<<ComboboxSelected>>")])
 
-
-        #Bouton Previous Song
-        #Affectation d'une image de bouton à une variable
-        self.previousSongImage = tkinter.PhotoImage(file="."+os.sep+"source"+os.sep+"view"+os.sep+"button"+os.sep+"sl1.png")
-        #Création du bouton
+        # Bouton Previous Song
+        # Affectation d'une image de bouton à une variable
+        self.previousSongImage = tkinter.PhotoImage(file="." + os.sep + "source" + os.sep + "view" + os.sep + "button" + os.sep + "sl1.png")
+        # Création du bouton
         self.previousSongButton = tkinter.Button(self)
-        #Placement du bouton
-        self.previousSongButton.grid(row = 3, column = 0, sticky="W")
-        #Quelques réglages d'apparence : couleur du background, épaisseur des bordures, et affectation de l'image au bouton
-        self.previousSongButton.config(image = self.previousSongImage, bd=0,bg='white', command= lambda:[self.previousSong()])
+        # Placement du bouton
+        self.previousSongButton.grid(row=3, column=0, sticky="W")
+        # Quelques réglages d'apparence : couleur du background, épaisseur des bordures, et affectation de l'image au bouton
+        self.previousSongButton.config(image=self.previousSongImage, bd=0, bg='white', command=lambda: [self.previousSong()])
         self.grid(row=3, column=0, pady=(100, 10))
 
-
-        #Bouton Play
-        #Affectation d'une image de bouton à une variable
-        self.playImage = tkinter.PhotoImage(file="."+os.sep+"source"+os.sep+"view"+os.sep+"button"+os.sep+"pl1.png")
-        #Création du bouton
+        # Bouton Play
+        # Affectation d'une image de bouton à une variable
+        self.playImage = tkinter.PhotoImage(file="." + os.sep + "source" + os.sep + "view" + os.sep + "button" + os.sep + "pl1.png")
+        # Création du bouton
         self.playButton = tkinter.Button(self)
-        #Placement du bouton
-        self.playButton.grid(row = 3, column = 0)
-        #Quelques réglages d'apparence : couleur du background, épaisseur des bordures, et affectation de l'image au bouton
-        self.playButton.config(image = self.playImage, bd=0,bg='white', command= lambda: [self.play()])
+        # Placement du bouton
+        self.playButton.grid(row=3, column=0)
+        # Quelques réglages d'apparence : couleur du background, épaisseur des bordures, et affectation de l'image au bouton
+        self.playButton.config(image=self.playImage, bd=0, bg='white', command=lambda: [self.play()])
 
-
-        #Bouton Pause
-        #Affectation d'une image de bouton à une variable
-        self.pauseImage = tkinter.PhotoImage(file="."+os.sep+"source"+os.sep+"view"+os.sep+"button"+os.sep+"pa1.png")
-        #Création du bouton
+        # Bouton Pause
+        # Affectation d'une image de bouton à une variable
+        self.pauseImage = tkinter.PhotoImage(file="." + os.sep + "source" + os.sep + "view" + os.sep + "button" + os.sep + "pa1.png")
+        # Création du bouton
         self.pauseButton = tkinter.Button(self)
-        #Placement du bouton
-        self.pauseButton.grid(row = 3, column =0, sticky="e")
-        #Quelques réglages d'apparence : couleur du background, épaisseur des bordures, et affectation de l'image au bouton
-        self.pauseButton.config(image = self.pauseImage, bd=0, bg='white', command = lambda: [self.pause()])
+        # Placement du bouton
+        self.pauseButton.grid(row=3, column=0, sticky="e")
+        # Quelques réglages d'apparence : couleur du background, épaisseur des bordures, et affectation de l'image au bouton
+        self.pauseButton.config(image=self.pauseImage, bd=0, bg='white', command=lambda: [self.pause()])
 
-
-        #Bouton Next Song
-        #Affectation d'une image de bouton à une variable
-        self.nextSongImage = tkinter.PhotoImage(file="."+os.sep+"source"+os.sep+"view"+os.sep+"button"+os.sep+"sr1.png")
-        #Création du bouton
+        # Bouton Next Song
+        # Affectation d'une image de bouton à une variable
+        self.nextSongImage = tkinter.PhotoImage(file="." + os.sep + "source" + os.sep + "view" + os.sep + "button" + os.sep + "sr1.png")
+        # Création du bouton
         self.nextSongButton = tkinter.Button(self)
-        #Placement du bouton
-        self.nextSongButton.grid(row = 3, column = 2)
-        #Quelques réglages d'apparence : couleur du background, épaisseur des bordures, et affectation de l'image au bouton
-        self.nextSongButton.config(image = self.nextSongImage, bd=0,bg='white', command = lambda:[self.nextSong()])
+        # Placement du bouton
+        self.nextSongButton.grid(row=3, column=2)
+        # Quelques réglages d'apparence : couleur du background, épaisseur des bordures, et affectation de l'image au bouton
+        self.nextSongButton.config(image=self.nextSongImage, bd=0, bg='white', command=lambda: [self.nextSong()])
 
-        #Un espace 
-        tkinter.Label(self,text=" ",bg="white").grid(row=6)
-        tkinter.Label(self,text=" ",bg="white").grid(row=2)
+        # Un espace
+        tkinter.Label(self, text=" ", bg="white").grid(row=6)
+        tkinter.Label(self, text=" ", bg="white").grid(row=2)
 
-
-        #Bouton Retour
-        #Creation du bouton retour, relié à la classe menu 
-        self.retourMenu = tkinter.Button(self, text="Retour", font = self.texte, command=lambda : [self.stop(),master.switch_frame()])
-        #Placement
-        self.retourMenu.grid(row=7,column=0, sticky="WS")
-        #Réglage de la bordure du bouton et du background
+        # Bouton Retour
+        # Creation du bouton retour, relié à la classe menu
+        self.retourMenu = tkinter.Button(self, text="Retour", font=self.texte, command=lambda: [self.stop(), master.switch_frame()])
+        # Placement
+        self.retourMenu.grid(row=7, column=0, sticky="WS")
+        # Réglage de la bordure du bouton et du background
         self.retourMenu.config(bd=1, bg="white")
 
-
-        #Bouton Supprimer les fichiers
-        #Création du bouton
-        self.deleteButton = tkinter.Button(self, text="Supprimer les fichiers", font = self.texte,  command = lambda:[self.supprimerFichiers()])
-        #Placement
-        self.deleteButton.grid(row = 7, column =2, sticky="W")
-        #Réglage du background
+        # Bouton acceder directement au dossier
+        # Création du bouton
+        self.deleteButton = tkinter.Button(self, text="Accéder au dossier", font=self.texte, command=lambda: [self.opendir()])
+        # Placement
+        self.deleteButton.grid(row=7, column=2, sticky="W")
+        # Réglage du background
         self.deleteButton.config(bg='white', bd=1)
 
+        # Bouton Supprimer les fichiers
+        # Création du bouton
+        self.deleteButton = tkinter.Button(self, text="Supprimer les fichiers", font=self.texte, command=lambda: [self.supprimerFichiers()])
+        # Placement
+        self.deleteButton.grid(row=8, column=2, sticky="W")
+        # Réglage du background
+        self.deleteButton.config(bg='white', bd=1)
 
         # mise à jour du répértoire des musiques        
         self.miseAJourRepertoire(self.parametres)
 
-
     def supprimerFichiers(self):
-        if(tkinter.messagebox.askyesno("Attention", "Voulez vraiment supprimer les fichiers ?")):
-            for filename in os.listdir(self.URL) :
+        if tkinter.messagebox.askyesno("Attention", "Voulez vraiment supprimer les fichiers ?"):
+            for filename in os.listdir(self.URL):
                 os.remove(self.URL + filename)
             self.miseAJourRepertoire(self.parametres)
 
-
-    def initLecteur(self):
-        #initialisation du lecteur
-        pygame.init()
-        pygame.mixer.init()
-
-
     def miseAJourRepertoire(self, parametres):
-        #parcours les fichiers du repertoire
+        # parcours les fichiers du repertoire
         self.parametres = parametres
-        self.URL = self.parametres["URL_Dossier"]+os.sep+"Resultat"+os.sep
-
+        self.URL = self.parametres["URL_Dossier"] + os.sep + "Resultat" + os.sep
 
         self.listeMusiques = os.listdir(self.URL)
 
-
-        self.comboTitres["values"]=self.listeMusiques #HERE
+        self.comboTitres["values"] = self.listeMusiques  # HERE
         self.comboTitres.set("")
         pygame.mixer.music.pause()
 
-
-
-        self.listeMusiques = [i for i in self.listeMusiques if ".mid" in i] #Filtre les fichiers pour n'avoir que du .mid
+        self.listeMusiques = [i for i in self.listeMusiques if ".mid" in i]  # Filtre les fichiers pour n'avoir que du .mid
         if len(self.listeMusiques) > 0:
             try:
-                pygame.mixer.music.load(self.URL+str(self.listeMusiques[0]))    #Charge le premier titre
+                pygame.mixer.music.load(self.URL + str(self.listeMusiques[0]))  # Charge le premier titre
             except pygame.error:
                 print("Erreur : fichier midi non valide")
             self.comboTitres.current(0)
 
-
-
     def play(self):
-        if(len(self.listeMusiques) == 0):
+        if len(self.listeMusiques) == 0:
             return
 
-        if(pygame.mixer.music.get_busy() == 0): #le lecteur ne joue rien
-            if(self.lecteurState == "Pause"):
+        if pygame.mixer.music.get_busy() == 0:  # le lecteur ne joue rien
+            if self.lecteurState == "Pause":
                 pygame.mixer.music.unpause()
 
-            if(self.lecteurState == "Stop" or self.lecteurState == "Play"):
+            if self.lecteurState == "Stop" or self.lecteurState == "Play":
                 pygame.mixer.music.play()
-
-
 
         self.lecteurState = "Play"
         return
 
     def pause(self):
-        if(len(self.listeMusiques) == 0):
+        if len(self.listeMusiques) == 0:
             return
 
-        if(self.lecteurState == "Play"):
+        if self.lecteurState == "Play":
             pygame.mixer.music.pause()
             self.lecteurState = "Pause"
         return
@@ -202,35 +183,46 @@ class Lecteur(tkinter.Frame):
         return
 
     def nextSong(self):
-        if(len(self.listeMusiques) == 0):
+        if len(self.listeMusiques) == 0:
             return
-        if self.lecteurState == "Play": # si un morceau est joué
-            pygame.mixer.music.stop() # on l'arrête
+        if self.lecteurState == "Play":  # si un morceau est joué
+            pygame.mixer.music.stop()  # on l'arrête
 
-        self.numeroTitre = (self.numeroTitre+1)%len(self.listeMusiques) # indice du prochain son à jouer
+        self.numeroTitre = (self.numeroTitre + 1) % len(self.listeMusiques)  # indice du prochain son à jouer
 
-        pygame.mixer.music.load(self.URL+str(self.listeMusiques[self.numeroTitre]))
+        pygame.mixer.music.load(self.URL + str(self.listeMusiques[self.numeroTitre]))
         pygame.mixer.music.play()
         self.lecteurState = "Play"
         self.comboTitres.current(self.numeroTitre)
 
     def previousSong(self):
-        if(len(self.listeMusiques) == 0):
+        if len(self.listeMusiques) == 0:
             return
-        if self.lecteurState == "Play": # si un morceau est joué
-            pygame.mixer.music.stop() # on l'arrête
+        if self.lecteurState == "Play":  # si un morceau est joué
+            pygame.mixer.music.stop()  # on l'arrête
 
-        self.numeroTitre = (self.numeroTitre-1)%len(self.listeMusiques) #indice du prochain son à jouer
+        self.numeroTitre = (self.numeroTitre - 1) % len(self.listeMusiques)  # indice du prochain son à jouer
 
-        pygame.mixer.music.load(self.URL+str(self.listeMusiques[self.numeroTitre]))
+        pygame.mixer.music.load(self.URL + str(self.listeMusiques[self.numeroTitre]))
         pygame.mixer.music.play()
         self.lecteurState = "Play"
         self.comboTitres.current(self.numeroTitre)
 
     def selectionMusique(self, event):
-        if self.lecteurState == "Play": #S si un morceau est joué
-            pygame.mixer.music.stop() # on l'arrête
-        pygame.mixer.music.load(self.URL+self.comboTitres.get()) #Charge le titre 
-        pygame.mixer.music.play() # on joue le morceau
+        if self.lecteurState == "Play":  # S si un morceau est joué
+            pygame.mixer.music.stop()  # on l'arrête
+        pygame.mixer.music.load(self.URL + self.comboTitres.get())  # Charge le titre
+        pygame.mixer.music.play()  # on joue le morceau
         self.lecteurState = "Play"
-        self.numeroTitre = self.comboTitres.current() #On met à jour le numero de titre joué
+        self.numeroTitre = self.comboTitres.current()  # On met à jour le numero de titre joué
+
+    def opendir(self):
+        print(self.URL)
+        dir = os.path.dirname(self.URL)
+        os.system('explorer ' + dir)
+
+
+def initLecteur():
+    # initialisation du lecteur
+    pygame.init()
+    pygame.mixer.init()
