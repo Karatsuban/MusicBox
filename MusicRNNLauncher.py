84badfee874ceeb2b662160f5a6e84bb30927672
YunfeiJIA1904 <77435576+YunfeiJIA1904@users.noreply.github.com>
2021-06-07 10:23:34 +0200

    Update MusicRNNLauncher.py
    changement l'ordre de lancement (maintenant install lance en 1er)

--- MusicRNNLauncher.py
+++ MusicRNNLauncher.py
@@ -1,8 +1,6 @@
 # !/usr/bin/python3
 #coding: utf-8
-from source.view.scripts import Application
 import Install
-
-if __name__ == "__main__":
-	Install.installRequiredPackages()
-	Application.start()
+Install.installRequiredPackages()
+from source.view.scripts import Application
+Application.start()
